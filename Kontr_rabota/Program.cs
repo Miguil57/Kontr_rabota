15857ae6fe132e03003976eac3a00ca0751f9ecd
Михаил Рожин <mihailrozin@Air-Mihail.HomeLAN>
2024-05-07 22:01:12 +0300

    @

--- Kontr_rabota/Program.cs
+++ Kontr_rabota/Program.cs
@@ -1,47 +1,35 @@
-﻿
-using System;
-
-namespace StringLengthFilter
+﻿string[] ArrayToArrayOf3OrlessLetters(string[] str)
 {
-    class Program
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
     {
-        static void Main(string[] args)
+        if (str[i].Length <= 3)
         {
-            // Define an initial string array
-            string[] inputArray = new[] { “Russia”, “Denmark”, “Kazan” };
-
-            // Filter the array by length
-            string[] filteredArray = FilterByLength(inputArray, 3);
-
-            // Print the results to the console
-            foreach (string s in filteredArray)
-            {
-                Console.WriteLine(s);
-            }
-        }
-
-        static string[] FilterByLength(string[] inputArray, int maxLength)
-        {
-            int count = 0;
-            for (int i = 0; i < inputArray.Length; i++)
-            {
-                if (inputArray[i].Length <= maxLength)
-                {
-                    count++;
-                }
-            }
-            string[] outputArray = new string[count];
-            int index = 0;
-            for (int i = 0; i < inputArray.Length; i++)
-            {
-                if (inputArray[i].Length <= maxLength)
-                {
-                    outputArray[index] = inputArray[i];
-                    index++;
-                }
-            }
-            return outputArray;
+            count++;
         }
     }
+    string[] result = new string[count];
+    int j = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            result[j] = str[i];
+            j++;
+        }
+    }
+    return result;
 }
+//функция записи для вывода массива строк на экран
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+string[] array = { "1234", "1567", "-2", "computer science" };
+string[] result = ArrayToArrayOf3OrlessLetters(array);
+PrintArray(result);
+
 
